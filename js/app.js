3cf7492faa56fe4dae1893eb103e887e4c3cbd0d
Petr Prášek <31828827+rteppwr@users.noreply.github.com>
2023-06-16 09:43:36 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById("dark-mode-btn");
-const body = document.querySelector("body");
-
-toggleBtn.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-});
